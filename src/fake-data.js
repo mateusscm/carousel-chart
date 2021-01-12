1f7e98eeebc45cdf8f16e3ceedb30c09ce403ad9
mateusscm <mateus.miranda@hotmail.com.br>
2021-01-12 00:43:45 -0300

    fake data to guide thoughts

---
+++ src/fake-data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 1,
+    type: "bar",
+    text: "Bar Chart",
+  },
+  {
+    id: 2,
+    type: "pie",
+    text: "Pie Chart",
+  },
+  {
+    id: 3,
+    type: "column",
+    text: "Column Chart",
+  },
+];
+
+export default data;
