72aadeec962516eafc1d4976816e8c1f69b5c019
mateus.m <mateus.m@juristecplus.com.br>
2021-01-14 19:26:29 -0300

    new icons path

---
+++ src/assets/icons/LeftArrow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function LeftArrow() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16.8 18L10.8 12L16.8 6.00002L15.6 3.60002L7.20005 12L15.6 20.4L16.8 18Z" fill="black"/>
+    </svg>
+  );
+}
+
+export default LeftArrow;
---
+++ src/assets/icons/RightArrow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function RightArrow() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.19995 18L13.2 12L7.19995 6.00002L8.39995 3.60002L16.8 12L8.39995 20.4L7.19995 18Z" fill="black"/>
+    </svg>
+  );
+}
+
+export default RightArrow;
