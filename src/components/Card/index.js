84c22af248de8ea207835eb022f196dbfeeaa4b5
mateus.m <mateus.m@juristecplus.com.br>
2021-01-14 19:32:59 -0300

    slider component changed with dots and new style

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { ChartBar, ChartLine, ChartPie } from "../Charts";
 import * as S from "./style";
 
-// import { Container } from './styles';
+function Card({ data, chartData }) {
+  const handleTypeChart = (type) => {
+    if (type === 'bar') {
+      return <ChartBar chartInfo={chartData.slice(1, 50)} />
+    } else if (type === 'column') {
+      return <ChartLine chartInfo={chartData.slice(1, 50)} />
+    } else {
+      return <ChartPie chartInfo={chartData.slice(1, 50)} />
+    }
+  }
 
-function Card({ data }) {
   return (
     <S.CardContainer>
-      <span>{data?.id}</span>
-      <span>{data?.type}</span>
+      {/* <span>{data?.id}</span>
+      <span>{data?.type}</span> */}
       <span className="badge">{data?.text}</span>
+      {handleTypeChart(data?.type)}
     </S.CardContainer>
   );
 }
--- src/components/Slider/style.js
+++ src/components/Slider/style.js
@@ -3,16 +3,19 @@ import styled, { css } from "styled-components";
 export const SliderSection = styled.section`
   display: flex;
   flex-direction: column;
+  position: relative;
+  padding: 0px 45px;
+  width: 100%;
 
   .slide {
     opacity: 0;
     transition-duration: 1s ease;
+    transform: translateX(0px);
   }
 
   .slide.active {
     opacity: 1;
     transition-duration: 1s;
-    transform: scale(1.08);
   }
 
   .container-dots {
@@ -21,6 +24,59 @@ export const SliderSection = styled.section`
     align-items: center;
     margin-top: 20px;
   }
+
+  button {
+    border: none;
+    display: flex;
+    align-items: center;
+    width: 30px;
+    justify-content: center;
+    height: 30px;
+    border-radius: 50%;
+    outline: none;
+    cursor: pointer;
+    background-color: blue;
+
+    &:hover {
+      background-color: darkblue;
+      box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+    0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
+    }
+
+    &:active {
+      background-color: darkblue;
+      box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+    0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
+    }
+
+    & svg > path {
+      fill: white;
+    }
+  }
+
+  .right {
+    position: absolute;
+    right: -13px;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    z-index: 10;
+
+    & > svg {
+      padding-left: 3px;
+    }
+  }
+
+  .left {
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    z-index: 10;
+
+    & > svg {
+      padding-right: 3px;
+    }
+  }
 `;
 
 export const Dots = styled.span`
@@ -28,17 +84,17 @@ export const Dots = styled.span`
   height: 10px;
   border-radius: 50%;
   margin: 0px 5px;
-  border: 1px solid #666;
   transition: transform 0.2s ease-in;
-  box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
-    0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
+  background-color: #666;
   cursor: pointer;
-
+  
   ${({ current }) =>
     current &&
     css`
       background-color: blue;
       border: 1px solid blue;
       transform: scale(1.2);
+      box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+        0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
     `}
 `;
