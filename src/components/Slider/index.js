5e766481069c0903213753546d60943890d7115d
mateus.m <mateus.m@juristecplus.com.br>
2021-01-14 19:32:17 -0300

    moving dots component to slider component

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,26 +1,35 @@
 import React from "react";
+import { LeftArrow, RightArrow } from "../../assets/icons";
 import Card from "../Card";
 import * as S from "./style";
+import chartData from '../../data.json';
 
-function Slider({ charts, current, setCurrent }) {
+function Slider({ charts, current, setCurrent, prevSlide, nextSlide }) {
   return (
     <S.SliderSection>
+        <button className="left" onClick={prevSlide}>
+          <LeftArrow />
+        </button>
+        <button className="right" onClick={nextSlide}>
+          <RightArrow />
+        </button>
       {charts.map((info, index) => {
         return (
           <div
             key={info?.id}
             className={index === current ? "slide active" : "slide"}
           >
-            {index === current && <Card data={info} />}
+            {index === current && <Card data={info} chartData={chartData} />}
           </div>
         );
       })}
       <div className="container-dots">
         {charts.map((idx) => {
-          console.log("idx", idx);
-          console.log("current", current);
+          // console.log("idx", idx);
+          // console.log("current", current);
           return (
             <S.Dots
+              key={idx?.id}
               current={idx.id - 1 === current}
               onClick={() => setCurrent(idx.id - 1)}
             />
